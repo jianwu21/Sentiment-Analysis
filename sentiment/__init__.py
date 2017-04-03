3206a55b64d931b20c60c459c8852dc6958bde40
Jagger Wu <xcb479@alumni.ku.dk>
2017-04-03 14:11:11 +0200

    update for __init__.py

--- sentiment/__init__.py
+++ sentiment/__init__.py
@@ -1,3 +1,9 @@
 """ Sentiment analysis framework, I like writing titles """
-from .utilities import (download_data, load_data,
-                        generate_word2vec, load_word2vec)
+from .utilities import (
+    download_data, 
+    load_data,
+    generate_word2vec, 
+    load_word2vec,
+    )
+
+from .lstm import LSTM
