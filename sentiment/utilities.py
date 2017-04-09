d133eb6dd5ba5dd2d7434a33aaad17b86c050975
Sotiris Lamprinidis <sot.lampr@gmail.com>
2017-04-09 18:02:18 +0200

    Function for generating test data

--- sentiment/utilities.py
+++ sentiment/utilities.py
@@ -9,6 +9,7 @@ import re
 import zipfile
 
 from gensim.models import Word2Vec
+from keras.preprocessing import sequence, text
 import numpy as np
 import pandas as pd
 
@@ -59,23 +60,27 @@ def load_data(path="./data"):
         pd.DataFrame(columns=["rating", "text"])
     """
     data = pd.DataFrame({'ratings': [], 'text': []})
-    for reviewer in REVIEWERS:
+    for i, reviewer in enumerate(REVIEWERS):
         ids = np.loadtxt(path + '/scaledata/' + reviewer + '/id.' + reviewer)
         files = ["%s/scale_whole_review/%s/txt.parag/%0.0f.txt" %
                  (path, reviewer, id)
                  for id in ids]
         ratings = np.loadtxt(path + '/scaledata/' + reviewer +
                              '/rating.' + reviewer)
+        labels = np.loadtxt(path + '/scaledata/' + reviewer +
+                             '/label.3class.' + reviewer)
+        reviewers = [i for j in range(len(ratings))]
         text_data = []
         for text_file in files:
             with open(text_file, encoding='latin-1') as fhandle:
                 text_data.append(fhandle.read())
 
-        data = data.append(pd.DataFrame({'ratings': ratings,
+        data = data.append(pd.DataFrame({'reviewers': reviewers,
+                                         'classes': labels,
+                                         'ratings': ratings,
                                          'text': text_data}),
                            ignore_index=True)
     return data
-
 
 
 def tokenize(sent):
@@ -114,14 +119,130 @@ def load_word2vec(fname="./data/w2vmodel"):
     return model.wv
 
 
-def text2vec(text_data, model=None):
+def text2vec(text_data, model=None, lim=0):
     """ Tokenization for doc
     Arguments:
         text_data (list of sentences)
+        model (word2vec model): trained model's vocabulary
+        lim (int): drop sentences with less than this number of tokens
     Returns:
         vectors transformed from the doc (list)
     """
     if model is None:
         model = load_word2vec()
-    return [np.array([model[w] for w in tokenize(x) if w in model.vocab])
+    vecs = [np.array([model[w] for w in tokenize(x) if w in model.vocab])
             for x in text_data]
+    return [v for v in vecs if v.shape[0] >= lim]
+
+
+def generate_dataset(data, token_lim=100, test_split=0.75,
+                     seed=43, pad=None, model=None):
+    """ Generate a train-test dataset ready for training.
+    Arguments:
+        data: (pd.DataFrame): data as given by the load_data function
+        token_lim (int): remove words with less than this many tokens
+        test_split (float): split to train / test sets
+        seed (int): Random seed argument for reliable random splitting
+        pad (int): Specific pad with zeros /truncate value. If None,
+            it is calculated mean(lengths) + 2 * std(lengths)
+        model (gensim Word2Vec vocabulary)
+    Returns:
+        X_train, y_train, X_test, y_test
+    """
+    if model is None:
+        model = load_word2vec()
+
+    vecs = [np.array([model[w] for w in tokenize(x) if w in model.vocab])
+            for x in data.text.values]
+    vals = data.ratings.values
+
+    idc = [i for i in range(len(vecs)) if vecs[i].shape[0] >= token_lim]
+
+    # Padding
+    if not isinstance(pad, int):
+        lens = [x.shape[0] for x in vecs]
+        pad = int(np.mean(lens) + 2 * np.std(lens))
+
+    padded = sequence.pad_sequences(vecs, maxlen=pad)
+
+    np.random.seed(seed)
+    perm = np.random.permutation(idc)
+
+    point = int(len(data) * test_split)
+    X_train, X_test = padded[perm[:point]], padded[perm[point:]]
+    y_train, y_test = vals[perm[:point]], vals[perm[point:]]
+
+    return X_train, y_train, X_test, y_test
+
+
+def newfunc(data, pad=600, holdout=.15, validation=.15, seed=42,
+            mode='both'):
+    """ Generate a train-validation-test dataset.
+    Arguments:
+        data: (pd.DataFrame): data as given by the load_data function
+        pad (int): Specific pad with zeros /truncate value.
+        houldout (float): Fration of data for holdout set
+        validation (float): Fration of data for validation set
+        seed (int): Random seed argument for reliable random splitting
+        mode (st): y values to return, one of 'rating', 'class', 'both'
+    Returns:
+        X_train, y_train, X_val, y_val, X_test, y_test, tokenizer
+    """
+    np.random.seed(seed)
+
+    # Remove the last 5% of the review
+    data.text = data.text.apply(lambda x: x[:-len(x)//20])
+
+    tkn = text.Tokenizer(6000)
+    tkn.fit_on_texts(data.text.values)
+    tokens = np.array(tkn.texts_to_sequences(data.text.values))
+
+    vals = data.ratings.values
+    labels = data.classes.values
+    reviewers = data.reviewers.values
+
+    # Get length of reviews in tokens
+    lens = np.array([len(x) for x in tokens])
+
+    # Remove reviews, ratings, labels, and reviewers for reviews
+    # 2 standard deviations smaller than the mean
+    filt = [lens >= (lens.mean() - 2*lens.std())]
+    tokens = tokens[filt]
+    vals = vals[filt]
+    labels = labels[filt]
+    reviewers = reviewers[filt]
+
+    if mode == 'class':
+        y_all = labels
+    elif mode == 'rating':
+        y_all == vals
+    else:
+        if mode != 'both':
+            print("Unrecognized option ", mode, " for 'mode' argument, "
+                  "returning both")
+        y_all = np.c_[vals, labels]
+
+    all_idc = np.arange(vals.shape[0])
+    idc_holdout = []
+    idc_validation = []
+    idc_train = []
+    for rating in np.unique(vals)[1:]:
+        idc_temp = all_idc[vals == rating]
+        N_holdout = int(holdout * len(idc_temp))
+        idc_holdout_temp = np.random.permutation(np.arange(idc_temp.size))
+        idc_holdout.extend(idc_temp[idc_holdout_temp][:N_holdout])
+        idc_temp = np.delete(idc_temp, idc_holdout_temp[:N_holdout])
+
+        N_validation = int(validation * len(idc_temp))
+        idc_validation_temp = np.random.permutation(np.arange(idc_temp.size))
+        idc_validation.extend(idc_temp[idc_validation_temp][:N_validation])
+        idc_temp = np.delete(idc_temp, idc_validation_temp[:N_validation])
+
+        idc_train.extend(idc_temp)
+
+    tokens = sequence.pad_sequences(tokens, pad)
+    X_train, y_train = tokens[idc_train], y_all[idc_train]
+    X_test, y_test = tokens[idc_holdout], y_all[idc_holdout]
+    X_val, y_val = tokens[idc_validation], y_all[idc_validation]
+
+    return X_train, y_train, X_val, y_val, X_test, y_test, tkn
